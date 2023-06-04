5c08324ec28b13e3b4224e0ee1b9aade1cec070f
just-vita <1254581982@qq.com>
2023-06-05 00:22:38 +0800

    创建初始项目

---
+++ src/test/java/top/vita/emos/wx/EmosWxApiApplicationTests.java
@@ -0,0 +1,13 @@
+package top.vita.emos.wx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmosWxApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
