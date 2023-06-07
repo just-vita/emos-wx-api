db802eb1ad9d183b0c60691784923faaa72e3608
just-vita <1254581982@qq.com>
2023-06-07 15:30:08 +0800

    3.2 实现项目启动时初始化系统常量数据

--- src/main/java/top/vita/emos/wx/EmosWxApiApplication.java
+++ src/main/java/top/vita/emos/wx/EmosWxApiApplication.java
@@ -1,11 +1,20 @@
 package top.vita.emos.wx;
 
+import cn.hutool.core.util.StrUtil;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import top.vita.emos.wx.config.SystemConstants;
+import top.vita.emos.wx.entity.Config;
+import top.vita.emos.wx.mapper.ConfigMapper;
+
+import javax.annotation.PostConstruct;
+import java.lang.reflect.Field;
+import java.util.List;
 
 @SpringBootApplication
 @MapperScan(basePackages = "top.vita.emos.wx.mapper")
@@ -13,8 +22,29 @@ import org.springframework.data.mongodb.repository.config.EnableMongoRepositorie
 @ServletComponentScan
 public class EmosWxApiApplication {
 
+    @Autowired
+    private SystemConstants systemConstants;
+    @Autowired
+    private ConfigMapper configMapper;
+
     public static void main(String[] args) {
         SpringApplication.run(EmosWxApiApplication.class, args);
     }
 
+    @PostConstruct
+    public void init() {
+        List<Config> configList = configMapper.selectAllParams();
+        configList.forEach(config -> {
+            String paramKey = config.getParamKey();
+            // 转为驼峰命名法
+            paramKey = StrUtil.toCamelCase(paramKey);
+            String paramValue = config.getParamValue();
+            try {
+                Field field = systemConstants.getClass().getDeclaredField(paramKey);
+                field.set(systemConstants, paramValue);
+            } catch (NoSuchFieldException | IllegalAccessException e) {
+                System.err.println(e.getMessage());
+            }
+        });
+    }
 }
--- src/main/java/top/vita/emos/wx/mapper/ConfigMapper.java
+++ src/main/java/top/vita/emos/wx/mapper/ConfigMapper.java
@@ -2,7 +2,10 @@ package top.vita.emos.wx.mapper;
 
 import org.apache.ibatis.annotations.Mapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Select;
 import top.vita.emos.wx.entity.Config;
+
+import java.util.List;
 
 /**
  * (Config)表数据库访问层
@@ -12,6 +15,7 @@ import top.vita.emos.wx.entity.Config;
  */
 @Mapper
 public interface ConfigMapper extends BaseMapper<Config> {
-
+    @Select("select param_key, param_value from sys_config where status = 1;")
+    List<Config> selectAllParams();
 }
 
