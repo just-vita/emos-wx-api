d0556148c0ce9391bb23bcb5ba3e1ca798a70a98
just-vita <1254581982@qq.com>
2023-06-09 18:12:49 +0800

    5.6 使用RabbitMQ实现异步接收消息

--- src/main/java/top/vita/emos/wx/task/MessageTask.java
+++ src/main/java/top/vita/emos/wx/task/MessageTask.java
@@ -1,18 +1,17 @@
 package top.vita.emos.wx.task;
 
-import com.rabbitmq.client.AMQP;
-import com.rabbitmq.client.Channel;
-import com.rabbitmq.client.Connection;
-import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.*;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 import top.vita.emos.wx.entity.MessageEntity;
+import top.vita.emos.wx.entity.MessageRefEntity;
 import top.vita.emos.wx.exception.EmosException;
 import top.vita.emos.wx.service.MessageService;
 
 import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @Author vita
@@ -52,4 +51,48 @@ public class MessageTask {
     public void sendAsync(String topic, MessageEntity entity) {
         send(topic, entity);
     }
+
+    public int receive(String topic) {
+        int i = 0;
+        try (Connection connection = factory.newConnection();
+             Channel channel = connection.createChannel();
+        ) {
+            channel.queueDeclare(topic, true, false, false, null);
+            while (true) {
+                final GetResponse response = channel.basicGet(topic, false);
+                if (response != null) {
+                    final Map<String, Object> headers = response.getProps().getHeaders();
+                    final String messageId = headers.get("messageId").toString();
+                    final byte[] body = response.getBody();
+                    final String message = new String(body);
+                    log.debug("从RabbitMQ接收的消息：" + message);
+
+                    final MessageRefEntity ref = new MessageRefEntity();
+                    ref.setMessageId(messageId);
+                    ref.setReceiverId(Integer.parseInt(topic));
+                    ref.setReadFlag(false);
+                    ref.setLastFlag(true);
+                    messageService.insertRef(ref);
+
+                    final long deliveryTag = response.getEnvelope().getDeliveryTag();
+                    channel.basicAck(deliveryTag, false);
+                    i++;
+                } else {
+                    break;
+                }
+
+            }
+        } catch (Exception e) {
+            log.error("执行异常", e);
+            throw new EmosException("接收消息失败");
+        }
+        return i;
+    }
+
+    @Async
+    public int receiveAsync(String topic) {
+        receive(topic);
+    }
+
+
 }
