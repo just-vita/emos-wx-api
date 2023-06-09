3accbb5acd9a87f92ac92b6702bd8ee2b814dac4
just-vita <1254581982@qq.com>
2023-06-10 00:41:52 +0800

    6.2 实现添加会议接口

--- src/main/java/top/vita/emos/wx/controller/MeetingController.java
+++ src/main/java/top/vita/emos/wx/controller/MeetingController.java
@@ -3,14 +3,17 @@ package top.vita.emos.wx.controller;
 
 import cn.hutool.core.date.DateTime;
 import cn.hutool.core.date.DateUtil;
+import cn.hutool.core.lang.UUID;
 import cn.hutool.json.JSONUtil;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.apache.shiro.authz.annotation.Logical;
 import org.apache.shiro.authz.annotation.RequiresPermissions;
 import top.vita.emos.wx.config.shiro.JwtUtils;
+import top.vita.emos.wx.controller.form.InsertMeetingForm;
 import top.vita.emos.wx.controller.form.SearchMyMeetingListByPageForm;
 import top.vita.emos.wx.controller.form.UpdateMeetingInfoForm;
+import top.vita.emos.wx.entity.Meeting;
 import top.vita.emos.wx.exception.EmosException;
 import top.vita.emos.wx.service.MeetingService;
 import org.springframework.web.bind.annotation.*;
@@ -51,6 +54,37 @@ public class MeetingController {
         map.put("length", length);
         ArrayList list = meetingService.searchMyMeetingListByPage(map);
         return R.ok().put("result", list);
+    }
+
+    @ApiOperation("添加会议")
+    @RequiresPermissions(value = {"ROOT", "MEETING:INSERT"}, logical = Logical.OR)
+    @PostMapping("/insertMeeting")
+    public R insertMeeting(@Valid @RequestBody InsertMeetingForm form, @RequestHeader("token") String token) {
+        if (form.getType() == 2 && (form.getPlace() == null || form.getPlace().length() == 0)) {
+            throw new EmosException("线下会议地点不能为空");
+        }
+        DateTime d1 = DateUtil.parse(form.getDate() + " " + form.getStart() + ":00");
+        DateTime d2 = DateUtil.parse(form.getDate() + " " + form.getEnd() + ":00");
+        if (d2.isBeforeOrEquals(d1)) {
+            throw new EmosException("结束时间必须大于开始时间");
+        }
+        if (!JSONUtil.isJsonArray(form.getMembers())) {
+            throw new EmosException("members不是JSON数组");
+        }
+        Meeting entity = new Meeting();
+        entity.setUuid(UUID.randomUUID().toString(true));
+        entity.setTitle(form.getTitle());
+        entity.setCreatorId((long) jwtUtil.getUserId(token));
+        entity.setDate(form.getDate());
+        entity.setPlace(form.getPlace());
+        entity.setStart(form.getStart() + ":00");
+        entity.setEnd(form.getEnd() + ":00");
+        entity.setType((short) form.getType());
+        entity.setMembers(form.getMembers());
+        entity.setDesc(form.getDesc());
+        entity.setStatus((short) 1);
+        meetingService.insertMeeting(entity);
+        return R.ok().put("result", "success");
     }
 
     @ApiOperation("更新会议")
